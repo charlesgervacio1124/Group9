02ff2cc0f23193dfc199785f94561b887f9b115a
charlesgervacio1124 <chdo.gervacio.up@phinmaed.com>
2026-01-20 15:09:37 +0800

    Fix merge conflict

---
+++ app/src/main/java/com/example/group9/view3.kt
@@ -0,0 +1,35 @@
+package com.example.group9
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class view3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_view3)
+        
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnBack = findViewById<Button>(R.id.button)
+        val btnNext = findViewById<Button>(R.id.button2)
+
+        btnBack.setOnClickListener {
+            finish()
+        }
+
+        btnNext.setOnClickListener {
+            val intent = Intent(this, view4::class.java)
+            startActivity(intent)
+        }
+    }
+}
